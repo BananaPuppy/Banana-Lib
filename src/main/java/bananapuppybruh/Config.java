693b43a0b3cdca27c9eeddc2e82b90c4c605a5e8
BananaPuppy <bananabaconpancakes07@gmail.com>
2024-11-23 15:27:54 -0800

    Update mod.json sources. Added json config helper object.

---
+++ src/main/java/bananapuppybruh/Config.java
@@ -0,0 +1,84 @@
+package bananapuppybruh;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Config {
+    private final Path configFilePath;
+    private final Map<String, ConfigOption<?>> options = new HashMap<>();
+
+    public Config(String configFileName) {
+        this.configFilePath = Paths.get(System.getProperty("user.dir"), "config", configFileName + ".json");
+        loadConfig();
+    }
+
+    public <T> void addOption(String key, T value) {
+        this.addOption(new ConfigOption<>(key, value));
+    }
+    public void addOption(ConfigOption<?> option) {
+        options.putIfAbsent(option.getKey(), option);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getOption(String key) {
+        ConfigOption<?> option = options.get(key);
+        if(option != null) {
+            return (T) option.getValue();
+        }
+        return null;
+    }
+
+    public <T> void updateOption(ConfigOption<T> option, T newValue) {
+        option.setValue(newValue);
+        serializeJson();
+    }
+
+    private void serializeJson() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Map<String, Object> serializedOptions = new HashMap<>();
+        for(ConfigOption<?> option : options.values()) {
+            serializedOptions.put(option.getKey(), option.getValue());
+        }
+        try(Writer writer = Files.newBufferedWriter(configFilePath)) {
+            gson.toJson(serializedOptions, writer);
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+
+    private void deserializeJson() {
+        Gson gson = new Gson();
+        try (Reader reader = Files.newBufferedReader(configFilePath)) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> loadedOptions = gson.fromJson(reader, HashMap.class);
+            if(loadedOptions != null) {
+                for(Map.Entry<String, Object> entry : loadedOptions.entrySet()) {
+                    options.put(entry.getKey(), new ConfigOption<>(entry.getKey(), entry.getValue()));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // Create a new config file.
+            serializeJson();
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+
+    private void loadConfig() {
+        if(Files.exists(configFilePath)) {
+            deserializeJson();
+        } else {
+            serializeJson();
+        }
+    }
+}
---
+++ src/main/java/bananapuppybruh/ConfigOption.java
@@ -0,0 +1,32 @@
+package bananapuppybruh;
+
+public class ConfigOption<T> {
+    private final String key;
+    private T value;
+
+    public ConfigOption(String key, T value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "ConfigOption{" +
+                "key='" + key + '\'' +
+                ", value=" + value +
+                ", type=" + value.getClass().getSimpleName() +
+                '}';
+    }
+}
